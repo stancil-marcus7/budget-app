7ab5e8aea8f45483bc116c6c986da0bf155a07a5
606682 <marcusstancil@c02yj0qpjgh7.users.bah.com>
2019-11-12 13:28:12 -0500

    Fix Jest stuff, now snapshots work

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,10 +1,13 @@
 import Header from '../../components/nav/Header' 
-import ReactShallowRenderer from 'react-test-renderer/shallow'
+import {shallow} from 'enzyme';
 import React from 'react'
-import { exportAllDeclaration } from '@babel/types';
 
-test('should render Header correctly', () => {
-    const renderer = new ReactShallowRenderer();
-    renderer.render(<Header />);
-    expect(renderer.getRenderOutput()).toMatchSnapshot();
+
+test(`should render header correctly`, () => {
+    const wrapper = shallow(<Header/>);
+    expect(wrapper).toMatchSnapshot();
+    // expect(wrapper.find(`h1`).text()).toBe(`Expensify`);
+    // const renderer = new ReactShallowRenderer();
+    // renderer.render(<Header/>);
+    // expect(renderer.getRenderOutput()).toMatchSnapshot();
 });
